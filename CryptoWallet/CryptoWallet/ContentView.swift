73dee74d13419bd90ec89edaf4f20453d3b119dd
Manobhav <54354723+Manubhav@users.noreply.github.com>
2024-05-01 10:13:24 +1000

    initial commit

---
+++ CryptoWallet/CryptoWallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoWallet
+//
+//  Created by Rishik  Sood on 01/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoWallet/CryptoWallet/CryptoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWalletApp.swift
+//  CryptoWallet
+//
+//  Created by Rishik  Sood on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
